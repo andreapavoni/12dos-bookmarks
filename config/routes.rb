aa96594ff744f076321eb1feae599bcdf2dc707e
Andrea Pavoni <andrea.pavoni@gmail.com>
2013-06-12 18:55:44 +0200

    add SiteController#index

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Bookmarks::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'site#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -40,7 +41,7 @@ Bookmarks::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
